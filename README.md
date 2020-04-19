c271e562678d99e93442e3c74deea555aa936c20
Richard Cook <rcook@rcook.org>
2020-04-18 20:45:24 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# absolute-path
+
+[![AppVeyor status for project](https://ci.appveyor.com/api/projects/status/XXX?svg=true)](https://ci.appveyor.com/project/rcook/absolute-path)
+[![AppVeyor status for master branch](https://ci.appveyor.com/api/projects/status/XXX/branch/master?svg=true)](https://ci.appveyor.com/project/rcook/absolute-path/branch/master)
+
+_Absolute paths in Rust_
+
+[Official home page][home]
+
+This is intended to be a cross-platform Rust function for creating absolute and normalized paths.
+
+## Building locally
+
+### Install Rust
+
+* [rustup][rustup] is recommended
+* Building with rustup has been tested on Linux, Windows and macOS
+
+### Clone workspace
+
+```bash
+cd /path/to/repos
+git clone https://gitlab.com/rcook/absolute-path.git
+cd /path/to/repos/absolute-path
+cargo build
+```
+
+## Licence
+
+[MIT License][licence]
+
+[home]: https://github.com/rcook/absolute-path
+[licence]: LICENSE
+[rustup]: https://rustup.rs/
---
+++ src/lib.rs
@@ -0,0 +1,49 @@
+use std::path::{Path, PathBuf};
+
+/// Normalize a target path to an absolute path relative to a base
+/// directory (typically the current working directory) without
+/// accessing the file system
+///
+/// # Arguments
+///
+/// * `base_dir` - Base directory (must be absolute), typically the current working directory
+/// * `target_path` - Target path
+///
+/// # Examples
+///
+/// ```
+/// ```
+pub fn absolute_path<P0: AsRef<Path>, P1: AsRef<Path>>(
+    base_dir: P0,
+    path: P1,
+) -> std::io::Result<PathBuf> {
+    Ok(base_dir.as_ref().join(path))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::absolute_path;
+
+    use std::path::Path;
+
+    #[test]
+    fn normalized_base_dir_relative_path_empty() {
+        check_absolute_path("/aa/bb/cc", "", "/aa/bb/cc/")
+    }
+
+    #[test]
+    fn normalized_base_dir_relative_path_single_part() {
+        check_absolute_path("/aa/bb/cc", "dd", "/aa/bb/cc/dd")
+    }
+
+    #[test]
+    fn normalized_base_dir_relative_path_multiple_parts() {
+        check_absolute_path("/aa/bb/cc", "dd/ee", "/aa/bb/cc/dd/ee")
+    }
+
+    fn check_absolute_path(p0: &str, p1: &str, expected: &str) {
+        let p = absolute_path(Path::new(p0), Path::new(p1)).unwrap();
+        assert!(p.is_absolute());
+        assert_eq!(p.to_str().unwrap(), expected)
+    }
+}
